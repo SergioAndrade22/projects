f6c2ab95f8d27d2af6af07718fb5e54e9a5c5f55
SergioAndrade22 <sergio.david2201@gmail.com>
2021-02-12 10:44:47 -0300

    Solved CORS configuration

--- Angular/note-app-back-end/src/main.ts
+++ Angular/note-app-back-end/src/main.ts
@@ -3,6 +3,23 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    credentials: false,
+    origin: [
+      'http://localhost:4200'
+    ],
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Access-Control-Request-Method',
+      'Authorization'
+    ]
+  });
+
   await app.listen(3000);
 }
 bootstrap();
