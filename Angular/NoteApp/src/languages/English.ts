3b842be03bb9e36c0ce5adc64b2bca0656e6054e
SergioAndrade22 <sergio.david2201@gmail.com>
2021-02-17 15:00:22 -0300

    Upgrade: created the basis for dynamic language change

---
+++ Angular/NoteApp/src/languages/English.ts
@@ -0,0 +1,10 @@
+export const English = {
+    home__header: 'This is a Google Keep clone project, with it I aim for the following:',
+    home__details: [
+        'Enhance my ability with Angular 2+',
+        'Develop the sufficient knowledge to plan small/medium projects ahead',
+        'Challenge myself as a fullstack developer',
+        'Gain a better understanding of NestJS and MySQL, the subjacent back-end technologies',
+        'Add funcionality that, under my consideration, Keep is missing'
+    ],
+}
---
+++ Angular/NoteApp/src/languages/Spanish.ts
@@ -0,0 +1,10 @@
+export const Spanish = {
+    home__header: 'Este es un clon de Google Keep, con él apunto a lo siguiente:',
+    home__details: [
+        'Mejorar mi habilidad con Angular 2+',
+        'Desarrollar el conocimiento y práctica necesaria para planear un desarrollo pequeño/mediano',
+        'Desafiarme como desarrollador fullstack',
+        'Obtener un mejor entendimiento de NestJS y MySQL, las tecnologías backend utilizadas',
+        'Añadir funcionalidades que, a mi parecer, faltan en Google Keep'
+    ],
+}
