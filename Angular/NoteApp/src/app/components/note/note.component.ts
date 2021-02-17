eb2a1231b86ee78a244db4eb6142e6739eecf31c
SergioAndrade22 <sergio.david2201@gmail.com>
2021-02-17 12:03:05 -0300

    Styling changes, bind notes values, added deletion

--- Angular/NoteApp/src/app/components/note/note.component.ts
+++ Angular/NoteApp/src/app/components/note/note.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { NotesService } from '../../services/notes.service';
+import { Note } from '../../core/models/note.model';
 
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.sass']
 })
-export class NoteComponent implements OnInit {
+export class NoteComponent implements OnChanges {
+  @Input() isCreate: boolean = false;
   @Input() isEdit: boolean = false;
-  @Input() noteTitle: string = '';
-  @Input() noteBody: string = '';
+  @Input() note: Note | undefined;
 
   noteForm = new FormGroup({
     title: new FormControl(''),
@@ -19,18 +20,24 @@ export class NoteComponent implements OnInit {
 
   constructor(private _notes: NotesService) {}
 
-  ngOnInit(): void {
-    document.getElementById('input__title')!.setAttribute('contenteditable', this.isEdit.toString());
-    document.getElementById('input__body')!.setAttribute('contenteditable', this.isEdit.toString());
-    if (this.noteTitle !== '') {
-      document.getElementById('input__title')!.innerText = this.noteTitle 
-    }
-    if (this.noteBody !== '') {
-      document.getElementById('input__body')!.innerText = this.noteBody
-    }
+  ngOnChanges(): void {
+    const input = document.getElementById('input__title')! as HTMLInputElement;
+    input.setAttribute('contenteditable', this.isEdit.toString());
+    if (this.note)
+      this.noteForm.controls['title'].setValue(this.note.title);
+
+    const textArea = document.getElementById('input__body')! as HTMLTextAreaElement;
+    textArea.setAttribute('contenteditable', this.isEdit.toString());
+    if (this.note)
+      this.noteForm.controls['body'].setValue(this.note.body);
   }
 
   saveNote(): void {
     this._notes.create(this.noteForm.value).subscribe((ret) => console.log(ret));
+  }
+
+  deleteNote(): void {
+    this._notes.remove(this.note!.id).subscribe();
+    location.reload()
   }
 }
--- Angular/NoteApp/src/app/components/notes/notes.component.ts
+++ Angular/NoteApp/src/app/components/notes/notes.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Note } from '../../core/models/note.model';
 import { NotesService } from '../../services/notes.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.sass']
 })
-export class NotesComponent implements OnInit {
-  notes: Note[] = []
+export class NotesComponent {
+  notes: Observable<Note[]>;
 
-  constructor(private _notes: NotesService) { }
-
-  ngOnInit(): void {
-    this._notes.findAll().subscribe((ret) => {this.notes = ret; console.log(ret);
-    });
+  constructor(private _notes: NotesService) { 
+    this.notes = this._notes.findAll();
   }
 
 }
