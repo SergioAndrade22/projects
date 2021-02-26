1530b972aab8e875938f812c4183257000d7e599
SergioAndrade22 <sergio.david2201@gmail.com>
2021-02-26 11:38:06 -0300

    Upgrade: added support for creating new categories, added confirmation dialogs

--- Angular/NoteApp/src/app/components/categories/categories.component.ts
+++ Angular/NoteApp/src/app/components/categories/categories.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Category } from 'src/app/core/models/category.model';
 import { CategoriesService } from '../../services/categories.service';
 import { Language } from '../../../languages/language.service';
+import { DialogService } from '../../shared/services/dialog.service';
 
 @Component({
   selector: 'app-categories',
@@ -14,19 +15,38 @@ export class CategoriesComponent {
   categories: Observable<Category[]>;
 
   constructor(private _categories: CategoriesService,
+              private _dialog: DialogService,
               public _language: Language) {
     this.categories = this._categories.findAll();
   }
 
+  newCategory(): void {
+    const dialogRef = this._dialog.getInputDialog(this._language.selectedLanguage.dialog.categoryCreate);
+    dialogRef.afterClosed().subscribe(name => {
+      this._categories.create({name}).subscribe();
+      location.reload();
+    });
+  }
+
   changeName(category: Category, name: string): void {
-    if(category.name !== name){
-      this._categories.update(category.id, {name}).subscribe();
-      location.reload()
+    if(category.name !== name) {
+      const dialogRef = this._dialog.getConfirmationDialog(this._language.selectedLanguage.dialog.save);
+      dialogRef.afterClosed().subscribe(result => {
+        if(result) {
+          this._categories.update(category.id, {name}).subscribe();
+          location.reload();
+        }
+      });
     }    
   }
 
   deleteCategory(category: Category): void {
-    this._categories.remove(category.id).subscribe();
+    const dialogRef = this._dialog.getConfirmationDialog(this._language.selectedLanguage.dialog.categoryDelete);
+    dialogRef.afterClosed().subscribe(result => {
+      if(result) {
+        this._categories.remove(category.id).subscribe();
+        location.reload();
+      }
+    });
   }
-
 }
