dc5f65d2096a08d06c8dd250b86e050b39e38991
SergioAndrade22 <sergio.david2201@gmail.com>
2021-02-26 11:38:22 -0300

    BugFix: solved concurrency issues due to double subscription

--- Angular/NoteApp/src/app/components/notes/notes.component.ts
+++ Angular/NoteApp/src/app/components/notes/notes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Note } from '../../core/models/note.model';
 import { NotesService } from '../../services/notes.service';
 import { Observable } from 'rxjs';
@@ -11,27 +11,26 @@ import { filter } from 'rxjs/operators';
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.sass']
 })
-export class NotesComponent {
-  notes: Observable<Note[]> = new Observable();
+export class NotesComponent implements OnInit{
+  notes: Note[] = [];
   params: Observable<Params> = new Observable();
 
   constructor(private _notes: NotesService,
               private _activatedRoute: ActivatedRoute,
               private _router: Router,
               public _language: Language) {
+    
+  }
+
+  ngOnInit() {
     this.params = this._activatedRoute.params;
     this.params.subscribe(params => {
-      if (params.state === 'active')
-        this.notes = this._notes.findAll();
-      else if (params.state === 'deleted')
-        this.notes = this._notes.findDeleted();
-      else {
-        this._router.events.pipe(filter(e => e instanceof NavigationStart)).subscribe(() => {
-          const state = this._router.getCurrentNavigation()!.extras!.state;
-          if(state)
-            this.notes = this._notes.findByCategory(state.id);
-        });
-      }
+      if (params.value === 'active')
+        this._notes.findAll().subscribe(notes => this.notes = notes);
+      else if (params.value === 'deleted')
+        this._notes.findDeleted().subscribe(notes => this.notes = notes);
+      else
+        this._notes.findByCategory(params.id).subscribe(notes => this.notes = notes);
     });
   }
 
